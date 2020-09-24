117eaa5b862f89f1aceed05654c9cd2534f7e1d5
ggiuliodori <ggiuliodori@kalateam.com.ar>
2020-09-24 09:33:03 -0300

    update doc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -23,6 +23,23 @@ module.exports = {
            ]
         },
         {
+          title: 'Sprint-1',
+          children: [ 
+            ['/docs/sprints/s001.md', 'Sprint 1']
+           ]
+        },{
+          title: 'Sprint-2',
+          children: [ 
+            ['/docs/sprints/s002.md', 'Sprint 2']
+           ]
+        },
+        {
+          title: 'Sprint-3',
+          children: [ 
+            ['/docs/sprints/s003.md', 'Sprint 3']
+           ]
+        },
+        {
           title: 'Pre-plataforma',
           children: [ 
             ['/docs/pre-plataforma/pp001.md', 'Funcionalidades']
@@ -39,9 +56,29 @@ module.exports = {
           children: [ 
             ['/docs/plataforma-docente/pd001.md', 'Funcionalidades']
            ]
+        },
+        {
+          title: 'Reglas de Negocio',
+          children: [ 
+            ['/docs/reglas/r001.md', 'Reglas de Negocio']
+           ]
         }
-      ]
-    }
+      ],
+    },
+    plugins: [
+      [
+        'vuepress-plugin-medium-zoom',
+        {
+          selector: '.theme-default-content img',
+          delay: 1000,
+          options: {
+            margin: 24,
+            background: '##333333',
+            scrollOffset: 0,
+          },
+        },
+      ],
+    ]
 }
 
   
